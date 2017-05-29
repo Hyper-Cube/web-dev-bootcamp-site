760ace8fc7a5804325b266d4804c4b4ba4b10377
Daniel Hill <daniel.hill13@gmail.com>
2017-05-29 14:44:35 -0700

    set up basic npm init, express, ejs engine, and hello world landing page

---
+++ index.js
@@ -0,0 +1,18 @@
+//declare dependencies here
+var express = require('express'),
+    app     = express();
+
+//App configuration
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+    res.render("landing");
+})
+
+// IF YOU DONT SEE THE CONSOLE LOG BELOW, make sure to set your environment variable for port.
+//To do this, in your terminal (in the project root folder) type "export PORT=3000" then after
+//Starting the app server, go to localhost:3000 in your browser
+    app.listen(process.env.PORT, process.env.IP, function(req, res){
+        console.log("Server started");
+    })
